14b6dac10d42b44430dcbd0d3247cc3a6c0c8fe6
Nilanjana Jui <nilanjana.csecu@gmail.com>
2025-02-06 23:58:30 +0600

    remove

--- R/Data Manipulation/LineChartCode.R
+++
@@ -1,9 +0,0 @@
-library(readxl)
-library(dplyr)
-
-Mydata <- read_excel("/home/nilanjana/2nd Semester/CSE-214/R/RProgrammingPractice/ColumnBarData.xlsx", sheet=1)
-
-cats <-Mydata$Years
-values <- Mydata$Sales
-
-plot(values, type="l", col = "red", lwd=2)
